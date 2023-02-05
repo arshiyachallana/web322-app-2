b83a00625472cde68a4f144865e6d1541421392b
Arshiya challana <achallana@myseneca.ca>
2023-02-05 17:36:10 +0530

    final app

---
+++ blog-service.js
@@ -0,0 +1,43 @@
+const fs = require("fs/promises");
+
+
+module.exports = class BlogSerice {
+    constructor() {
+        this.posts = null;
+        this.categories = null;
+    }
+    initialize() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const posts = await fs.readFile('./data/posts.json', { encoding: 'utf8' });
+                const categories = await fs.readFile('./data/categories.json', { encoding: 'utf8' });
+                this.posts = JSON.parse(posts);
+                this.categories = JSON.parse(categories);
+                resolve()
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+    async getAllPosts() {
+        if (this.posts?.length > 0) {
+            return this.posts;
+        } else {
+            return Promise.reject(new Error('no results returned'));
+        }
+    }
+    async getPublishedPosts() {
+        if (this.posts?.length > 0) {
+            return this.posts.filter((e) => e.published === true);
+        } else {
+            return Promise.reject(new Error('no results returned'));
+        }
+    }
+    async getCategories() {
+        if (this.categories?.length > 0) {
+            return this.categories;
+        } else {
+            return Promise.reject(new Error('no results returned'));
+        }
+    }
+}
--- server.js
+++ server.js
@@ -1,19 +1,58 @@
-/********************************************************************************* 
-*  WEB322 – Assignment 2
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
-*  No part of this assignment has been copied manually or electronically from any other source 
-*  (including web sites) or distributed to other students. 
-*  
-*  Name: Arshiya challana  Student ID: 154101216 Date: 
-********************************************************************************/
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ______________________ Student ID: ______________ Date: ________________
+*
+*  Cyclic Web App URL: ________________________________________________________
+*
+*  GitHub Repository URL: ______________________________________________________
+*
+********************************************************************************/ 
 var HTTP_PORT = 8080;
 var express = require("express");
 var app = express();
 const path = require('path');
 const router = express.Router();
 app.use(express.static('public'))
+const BlogSerice = require("./blog-service")
+const BS = new BlogSerice()
 router.get("/", (req, res) => {
+    res.redirect("/about");
+});
+router.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname + '/views/about.html'));
 });
-app.use('/', router);
-app.listen(HTTP_PORT);
+router.get("/blog", (req, res) => {
+    BS.getPublishedPosts().then((data) => {
+        res.json({ data: data });
+    }).catch((err) => {
+        res.send({ message: err?.message });
+    });
+});
+router.get("/posts", (req, res) => {
+    BS.getAllPosts().then((data) => {
+        res.json({ data: data });
+    }).catch((err) => {
+        res.send({ message: err?.message });
+    });
+});
+router.get("/categories", (req, res) => {
+    BS.getCategories().then((data) => {
+        res.json({ data: data });
+    }).catch((err) => {
+        res.send({ message: err?.message });
+    });
+});
+BS.initialize().then(() => {
+    app.use('/', router);
+    app.listen(HTTP_PORT);
+    app.all('*', function (req, res) {
+        res.sendFile(path.join(__dirname + '/views/404Error.html'));
+    });
+}).catch((err) => {
+    console.log("err", err);
+    res.sendFile(path.join(__dirname + '/views/404Error.html'));
+})
+
