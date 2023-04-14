1b344fcf984efc8ca492a7a54465d9e8c92dc908
Arshiya challana <achallana@myseneca.ca>
2023-04-15 00:59:20 +0530

    change server js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 05
+*  WEB322 – Assignment 06
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Arshiya challana Student ID: 154101216 Date: 27/03/2023
+*  Name: Arshiya challana Student ID: 154101216 Date: 14/04/2023
 *
 *  Cyclic Web App URL: https://sore-plum-centipede-hat.cyclic.app
 *
